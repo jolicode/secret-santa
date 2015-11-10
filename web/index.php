fdd9ea47fbf4d229a26df7f37e40cf09f4a0e43f
Damien Alexandre <mobman02@gmail.com>
2015-11-10 14:09:19 +0100

    Oups

--- web/index.php
+++ web/index.php
@@ -14,7 +14,11 @@ $provider = new Bramdevries\Oauth\Client\Provider\Slack([
 
 if (!isset($_GET['code'])) {
     // If we don't have an authorization code then get one
-    $authUrl = $provider->getAuthorizationUrl();
+    $options = [
+        'scope' => ['chat:write:bot', 'users:read'] // array or string
+    ];
+
+    $authUrl = $provider->getAuthorizationUrl($options);
     $_SESSION['oauth2state'] = $provider->getState();
     header('Location: '.$authUrl);
     exit;
