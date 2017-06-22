3e9304e339194d1980ba225e44c8ad6765fe0570
Loïck Piera <pyrech@gmail.com>
2017-06-22 21:55:03 +0200

    Add monolog and fix a deprecation 😬

--- etc/bundles.php
+++ etc/bundles.php
@@ -4,4 +4,5 @@ return [
     'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
     'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
     'Symfony\Bundle\WebProfilerBundle\WebProfilerBundle' => ['dev' => true, 'test' => true],
+    'Symfony\Bundle\MonologBundle\MonologBundle' => ['all' => true],
 ];
