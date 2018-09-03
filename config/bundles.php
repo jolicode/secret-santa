3dd69ae8a529795543d8cbdcf9879221f2e39b4f
Loïck Piera <pyrech@gmail.com>
2018-09-03 14:00:27 +0200

    Fix CSP for images and inline js and install nelmio/security-bundle for more security - fix #73

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Nelmio\SecurityBundle\NelmioSecurityBundle::class => ['all' => true],
 ];
