930f72bee404a70265e39d728d5adde21e30f391
Loïck Piera <pyrech@gmail.com>
2019-10-06 00:45:25 +0200

    Keep stats per application

--- src/Statistic/StatisticCollector.php
+++ src/Statistic/StatisticCollector.php
@@ -31,7 +31,9 @@ class StatisticCollector
 
         // If the key does not exist, it is set to 0 before performing the operation
         $this->client->incr("stats:month:$currentYear-$currentMonth");
+        $this->client->incr("stats:month-app:$currentYear-$currentMonth-$applicationCode");
         $this->client->incr("stats:year:$currentYear");
+        $this->client->incr("stats:year-app:$currentYear-$applicationCode");
         $this->client->incr("stats:app:$applicationCode");
         $this->client->incr('stats:total');
     }
@@ -40,7 +42,9 @@ class StatisticCollector
     {
         $counters = [
             'month' => [],
+            'month-app' => [],
             'year' => [],
+            'year-app' => [],
             'app' => [],
             'total' => 0,
         ];
@@ -60,6 +64,26 @@ class StatisticCollector
 
             ksort($counters['month']);
             ksort($counters['year']);
+        }
+
+        foreach (['year', 'month'] as $stat) {
+            $counter = [];
+            foreach ($counters[$stat] as $key => $value) {
+                $counter[$key] = [
+                    'total' => $value,
+                    'applications' => [],
+                ];
+                foreach ($counters['app'] as $applicationCode => $applicationTotal) {
+                    $counter[$key]['applications'][$applicationCode] =
+                        !empty($counters["$stat-app"])
+                        && !empty($counters["$stat-app"]["$key-$applicationCode"])
+                            ? $counters["$stat-app"]["$key-$applicationCode"]
+                            : 0;
+                }
+            }
+            $counters[$stat] = $counter;
+            $counters["$stat-max"] = max(array_column($counter, 'total'));
+            unset($counters["$stat-app"]);
         }
 
         return $counters;
