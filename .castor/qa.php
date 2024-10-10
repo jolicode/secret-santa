fc3ba17b410dcaf052751cb8bd291da73ad2b59e
Loïck Piera <pyrech@gmail.com>
2024-10-10 09:45:49 +0200

    Update infrastructure

--- .castor/qa.php
+++ .castor/qa.php
@@ -1,53 +1,64 @@
 <?php
 
-/*
- * This file is part of the Secret Santa project.
- *
- * (c) JoliCode <coucou@jolicode.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace qa;
 
 use Castor\Attribute\AsTask;
 
+use function Castor\io;
+use function Castor\variable;
+use function docker\docker_compose_run;
+use function docker\docker_exit_code;
+
 #[AsTask(description: 'Runs all QA tasks')]
-function all(): void
+function all(): int
 {
     install();
-    cs();
-    phpstan();
-    phpunit();
+    $cs = cs();
+    $phpstan = phpstan();
+    $phpunit = phpunit();
+
+    return max($cs, $phpstan , $phpunit);
 }
 
 #[AsTask(description: 'Installs tooling')]
 function install(): void
 {
-    docker_compose_run('composer install --working-dir tools/php-cs-fixer');
-    docker_compose_run('composer install --working-dir tools/phpstan');
+    io()->title('Installing QA tooling');
+
+    docker_compose_run('composer install -o', workDir: '/var/www/tools/php-cs-fixer');
+    docker_compose_run('composer install -o', workDir: '/var/www/tools/phpstan');
 }
 
-#[AsTask(description: 'Runs PHPStan')]
-function phpstan(): void
+#[AsTask(description: 'Runs PHPUnit', aliases: ['phpunit'])]
+function phpunit(): int
 {
-    docker_compose_run('bin/console about --env=test'); // to create the cache
-    docker_compose_run('phpstan');
+    return docker_exit_code('phpunit');
 }
 
-#[AsTask(description: 'Runs PHPUnit')]
-function phpunit(): void
+#[AsTask(description: 'Runs PHPStan', aliases: ['phpstan'])]
+function phpstan(): int
 {
-    docker_compose_run('bin/phpunit');
-}
+    if (!is_dir(variable('root_dir') . '/tools/phpstan/vendor')) {
+        io()->error('PHPStan is not installed. Run `castor qa:install` first.');
 
-#[AsTask(description: 'Fixes Coding Style')]
-function cs(bool $dryRun = false): void
-{
-    if ($dryRun) {
-        docker_compose_run('php-cs-fixer fix --dry-run --diff');
-    } else {
-        docker_compose_run('php-cs-fixer fix');
+        return 1;
     }
+
+    return docker_exit_code('phpstan', workDir: '/var/www');
+}
+
+#[AsTask(description: 'Fixes Coding Style', aliases: ['cs'])]
+function cs(bool $dryRun = false): int
+{
+    if (!is_dir(variable('root_dir') . '/tools/php-cs-fixer/vendor')) {
+        io()->error('PHP-CS-Fixer is not installed. Run `castor qa:install` first.');
+
+        return 1;
+    }
+
+    if ($dryRun) {
+        return docker_exit_code('php-cs-fixer fix --dry-run --diff', workDir: '/var/www');
+    }
+
+    return docker_exit_code('php-cs-fixer fix', workDir: '/var/www');
 }
