482629fd2452c8257c14dcd75932f33aed760fd0
Damien ALEXANDRE <dalexandre@jolicode.com>
2025-11-26 10:08:15 +0100

    Update bugsnag recipe

--- config/bundles.php
+++ config/bundles.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the Secret Santa project.
- *
- * (c) JoliCode <coucou@jolicode.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
